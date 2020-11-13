bc9fbbdade54233786c6144e569b0e417725bcb7
Matt Gula <gulatime75@gmail.com>
2020-11-12 23:44:13 -0500

    Fix error in error string

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ def card_creator():
         return jsonify(image_path=card_image_path,error=error,player_stats=player_stats_data)
 
     except Exception as err:
-        error = f"err"
+        error = f"{err}"
         return jsonify(image_path=None,error=error,player_stats=None)
 
 @app.route('/upload', methods=["POST","GET"])
