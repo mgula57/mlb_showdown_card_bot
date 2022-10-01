693e65e64f15726fdf067e903abcc7b1da7f5801
Matthew Gula <mgula57@gmail.com>
2022-10-01 10:26:25 -0500

    Add V1 of Explore tab

---
+++ static/front_end/tableau_3_2.js
@@ -0,0 +1,27 @@
+
+// CAPTURES CURRENT WIDTH OF SCREEN
+// USED TO DECIDE DESKTOP/MOBILE
+function getWidth() {
+return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+);
+}
+
+// ADD WIDTH AND HEIGHT
+var divElement = document.getElementById('viz1664118452778');
+var vizElement = divElement.getElementsByTagName('object')[0];
+vizElement.style.width = '100%';
+vizElement.style.height = '900px';
+
+// ADD PARAM TO FORCE EITHER DESKTOP OR MOBILE LAYOUT
+var deviceElementvizElement = document.getElementById('deviceParam')
+deviceElementvizElement.value = getWidth() < 600 ? "mobile" : "desktop";
+
+// ADD TABLEAU JS
+var scriptElement = document.createElement('script');
+scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';
+vizElement.parentNode.insertBefore(scriptElement, vizElement);
