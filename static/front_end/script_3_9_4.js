cad1e9fc5ded65967406e8f8e1e61b59eac611f0
Matt Gula <mgula57@gmail.com>
2025-04-28 20:43:16 -0500

    Add scroll to ability on status button

--- static/front_end/script_3_9_4.js
+++ static/front_end/script_3_9_4.js
@@ -654,7 +654,7 @@ function showCardData(data) {
             submessage=`${data.player_name} ${data.player_year}`, 
             iconClasses="fa-solid fa-circle-check", 
             backgroundColor="var(--success-color)",
-            removeAfterSeconds=2
+            removeAfterSeconds=2.5
         );
         
         // ADD CHILDREN TO PLAYER DETAILS DIV
