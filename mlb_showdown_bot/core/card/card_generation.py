69da74786a3aba7ecc40161f5a2801929f5f4aa6
Matt Gula <mgula57@gmail.com>
2026-04-24 20:03:59 -0500

    Add more context to error message for player not found

--- mlb_showdown_bot/core/card/card_generation.py
+++ mlb_showdown_bot/core/card/card_generation.py
@@ -188,7 +188,7 @@ def generate_card(**kwargs) -> dict[str, Any]:
                     search_name = search_name.split('(')[0].strip()
                 player_data = mlb_stats_api.build_full_player_from_search(search_name=search_name, stats_period=stats_period, league=league)
                 if player_data is None:
-                    raise Exception(f"Player not found in MLB API with name: {kwargs.get('name', '')} and year: {kwargs.get('year', '')} in the {league}.")
+                    raise Exception(f"Player not found in MLB API with name: {kwargs.get('name', '')} and year: {kwargs.get('year', '')} in the {league}. Check spelling or try using the player's MLB ID from the URL on MLB.com as the name instead. Ex: https://www.mlb.com/player/aaron-judge-592450 would have a player ID of 592450.")
                 normalized_player_stats = PlayerStatsNormalizer.from_mlb_api(player=player_data, stats_period=stats_period)
 
                 if normalized_player_stats is None or normalized_player_stats.PA is None or normalized_player_stats.PA == 0:
