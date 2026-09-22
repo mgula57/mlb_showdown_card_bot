0f56b377cf72de9c842f81390a049404665a5687
Matt Gula <mgula57@gmail.com>
2026-09-21 22:23:50 -0500

    Add pitcher sit codes

--- mlb_showdown_bot/core/mlb_stats_api/clients/people_client.py
+++ mlb_showdown_bot/core/mlb_stats_api/clients/people_client.py
@@ -199,7 +199,7 @@ class PeopleClient(BaseMLBClient):
                 hitter_stat_groups = ['hitting', 'fielding']
                 league_list_hydration = f",leagueListId={league_list.value}" if league_list else ""
                 is_milb = league_list and 'milb' in league_list.value.lower()
-                sit_codes = ['sp', 'rp'] if type == PlayerType.PITCHER else []
+                sit_codes = ['sp', 'rp'] # Used for IP/GS
                 if additional_sit_codes:
                     sit_codes.extend(additional_sit_codes)
                 sit_code_str = f",sitCodes=[{','.join(sit_codes)}]" if sit_codes and len(sit_codes) > 0 and not is_milb else ''
@@ -223,7 +223,7 @@ class PeopleClient(BaseMLBClient):
 
             if index > 0:  # Add a short delay between requests to avoid hitting rate limits
                 sleep(0.15)
-
+        
         return Players(people=all_players)
 
     # -----------------------
